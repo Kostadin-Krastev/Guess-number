78582b4807c58dbc7002464eb7f37f20aa856ee1
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-12-13 14:42:02 +0200

    add some querySelector to manipulate the text

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.result-number').textContent = 14;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('input').value = 23;
+console.log(document.querySelector('input').value);
+
+// Just a test for my ONMAG.bg project
+// document.querySelector('input').placeholder = 'Some new text for testing';
