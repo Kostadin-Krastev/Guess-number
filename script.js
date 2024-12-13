071b078d9f1013f9dc2639c0e6a42ececff5508c
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-12-13 15:34:50 +0200

    add eventListener to button Chek!

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// add event listener to button Check!
+document.querySelector('.check-number').addEventListener('click', function () {
+  const guessNumber = Number(document.querySelector('input').value);
+  console.log(guessNumber, typeof guessNumber);
 
-document.querySelector('.result-number').textContent = 14;
-document.querySelector('.score').textContent = 10;
+  if (!guessNumber) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
 
-document.querySelector('input').value = 23;
-console.log(document.querySelector('input').value);
+// Code for learning and testing
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.result-number').textContent = 14;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('input').value = 23;
+// console.log(document.querySelector('input').value);
 
 // Just a test for my ONMAG.bg project
 // document.querySelector('input').placeholder = 'Some new text for testing';
