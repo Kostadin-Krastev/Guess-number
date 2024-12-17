6f8be45db83b1658225f9e6e11fc7f2fbbe28ef1
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-12-17 15:33:38 +0200

    clear old commented code

--- script.js
+++ script.js
@@ -40,16 +40,3 @@ document.querySelector('.check-number').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   }
 });
-
-// Code for learning and testing
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-// document.querySelector('.result-number').textContent = 14;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('input').value = 23;
-// console.log(document.querySelector('input').value);
-
-// Just a test for my ONMAG.bg project
-// document.querySelector('input').placeholder = 'Some new text for testing';
