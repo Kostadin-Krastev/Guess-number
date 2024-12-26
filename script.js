82ce9b1192c89ed6b7138961ddec699bd7326702
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-12-26 10:57:14 +0200

    changing background color of the <body> and width of the .result-number when the player wins

--- script.js
+++ script.js
@@ -17,26 +17,36 @@ document.querySelector('.check-number').addEventListener('click', function () {
 
   // Creating logic of the game - if the number player input is correct or not
   if (!guessNumber) {
+    // When there is no input
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guessNumber > randomNumber) {
+    // Scenario when the guess number is too high
     if (scoreNumber > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
       scoreNumber--;
       document.querySelector('.score').textContent = scoreNumber;
+      // When player lost
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
   } else if (guessNumber < randomNumber) {
     if (scoreNumber > 1) {
+      // Scenario when the guess number is too low
       document.querySelector('.message').textContent = '📉 Too low!';
       scoreNumber--;
       document.querySelector('.score').textContent = scoreNumber;
+      // When player lost
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+    // When player wins
   } else if (guessNumber === randomNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    // Changing background color
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    // Changing the with of the div for the Result number
+    document.querySelector('.result-number').style.width = '30rem';
   }
 });
